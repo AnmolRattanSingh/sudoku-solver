5acecee5f808265fd1414d760f2523a2d698c61f
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-04-24 10:53:21 -0400

    added initial Board representation class

---
+++ board.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+class Board:
+    def __init__(self, puzzle):
+        """
+        Initialize the board with a np.array representing a
+        9x9 sudoku puzzle.
+
+        args:
+            puzzle(list): a list of 81 integers, where 0 represents an empty cell
+        """
+        self.grid = np.array(puzzle).reshape((9, 9))
+    
+    def __repr__(self):
+        """
+        Return a string representation of the board.
+        """
+        return str(self.grid)
+        
+    def get_val(self, row, col):
+        """
+        Get the value at a given row and column.
+
+        args:
+            row(int): the row of the cell between 0 and 8
+            col(int): the column of the cell between 0 and 8
+
+        returns:
+            (int) the value at the given row and column
+        """
+        try:
+            return self.grid[row][col]
+        except IndexError:
+            raise IndexError(f"Invalid row or column: {row}, {col}")
+    
+    def set_val(self, row, col, val):
+        """
+        Set the value at a given row and column.
+
+        args:
+            row(int): the row of the cell between 0 and 8
+            col(int): the column of the cell between 0 and 8
+            val(int): the value to set the cell to, between 1 and 9
+        """
+        if val in range(1, 10):
+            self.grid[row][col] = val
+        else:
+            raise ValueError(f"Invalid value: {val}")
+        
+    def get_row(self, row):
+        """
+        Get the values in a given row.
+
+        args:
+            row(int): the row of the grid between 0 and 8
+
+        returns:
+            (numpy.ndarray) the values in the given row
+        """
+        try:
+            return self.grid[row]
+        except IndexError:
+            raise IndexError(f"Invalid row: {row}")
+    
+    def get_col(self, col):
+        """
+        Get the values in a given column.
+
+        args:
+            col(int): the column of the grid between 0 and 8
+
+        returns:
+            (numpy.ndarray) the values in the given column
+        """
+        return self.grid[:, col]
