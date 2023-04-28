25b1c6e6b888393fd1c8027034931f3db189be50
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-04-28 16:35:00 -0400

    added more tests

--- test_board.py
+++ test_board.py
@@ -4,51 +4,157 @@ Tests for the data analysis functions
 import pytest
 from board import Board
 
-GET_NUM_CASES = [
-    # test that order of number is from left to right
-    ("1j2k3l4k5", 12345),
+PUZZLE = [0, 0, 3, 0, 2, 0, 6, 0, 0,
+          9, 0, 0, 3, 0, 5, 0, 0, 1,
+          0, 0, 1, 8, 0, 6, 4, 0, 0,
+          0, 0, 8, 1, 0, 2, 9, 0, 0,
+          7, 0, 0, 0, 0, 0, 0, 0, 8,
+          0, 0, 6, 7, 0, 8, 2, 0, 0,
+          0, 0, 2, 6, 0, 9, 5, 0, 0,
+          8, 0, 0, 2, 0, 3, 0, 0, 9,
+          0, 0, 5, 0, 1, 0, 3, 0, 0]
 
-    # test that correct number is returned when whitespaces are present
-    ("kj k23 122", 23122),
+TEST_BOARD = Board(PUZZLE)
 
-    # test that an already clean string comes out the same
-    ("12345", 12345),
+GET_VAL_CASES = [
+    # Test value at valid row and column
+    (3, (0, 2)),
+    # Test value at invalid row
+    (9, (1, 0)),
 ]
 
-@pytest.mark.parametrize("get_num_test, get_num_result", GET_NUM_CASES)
-def test_get_num(get_num_test, get_num_result):
-    """
-    Test the get_num function which filters out numbers in a string.
-    """
-    assert get_num(get_num_test) == get_num_result
+GET_VAL_EXCEPTION_CASES = [
+    # Test value at invalid column
+    (IndexError, (0, 10)),
+    (IndexError, (3, 11)),
+]
 
+SET_VAL_CASES = [
+    # Test that value at valid row and column is set appropriately
+    (1, (0, 2)),
+    (2, (1, 0)),
+]
 
-@pytest.mark.parametrize("genres_test, genres_result", GENRES_CASES)
-def test_genres_over_career(genres_test, genres_result):
-    """
-    Test output of genres_over_career function which returns a
-    dictionary containing an actor's movie genre information.
-    """
-    assert genres_test == genres_result
+SET_VAL_EXCEPTION_CASES = [
+    # Test that value at invalid row raises an exception
+    (IndexError, (0, 12)),
+    (IndexError, (3, 13)),
+]
 
+FIXED_VAL_CASES = [
+    # Test that 0 in initial puzzle is converted to 0 in fixed value
+    (0, (3, 1)),
+    (0, (5, 4)),
+    # Test that number in initial puzzle is converted to 1 in fixed value
+    (1, (0, 2)),
+    (1, (8, 4)),
+]
 
-@pytest.mark.parametrize("box_office_test, box_office_result", BOX_CASES)
-def test_box_office_over_time(box_office_test, box_office_result):
-    """
-    Test output of box_office_over_time function which returns a
-    dictionary containing box office collection of actor per year.
-    """
-    assert box_office_test == box_office_result
+GET_ROW_CASES = [
+    # Test that a valid row can be accessed and all values are correct
+    ([0, 0, 8, 1, 0, 2, 9, 0, 0], 3),
+    ([0, 0, 2, 6, 0, 9, 5, 0, 0], 6),
+]
 
-    # test years in the dictionary keys are sorted correctly
-    assert int(list(box_office_test_dict.keys())[0].split("–")[
-               0]) <= int(list(box_office_test_dict.keys())[-1].split("–")[0])
+GET_COL_CASES = [
+    # Test that a valid column can be accessed and all values are correct
+    ([0, 9, 0, 0, 7, 0, 0, 8, 0], 0),
+    ([0, 0, 0, 0, 0, 0, 0, 0, 0], 1),
+]
+    
+GET_SUBGRID_CASES = [
+    # Test that a valid subgrid can be accessed and all values are correct
+    ([0, 0, 3, 9, 0, 0, 0, 0, 1,], (0, 0)),
+    ([0, 0, 3, 0, 1, 8, 0, 8, 1], (1, 1)),
+]
 
+def test_init_shape():
+    """
+    Test that board initializes to the correct shape
+    """
+    assert Board(PUZZLE).grid.shape == (9, 9)
 
-@pytest.mark.parametrize("ratings_test, ratings_result", RATINGS_CASES)
-def test_movie_ratings_over_time(ratings_test, ratings_result):
+@pytest.mark.parametrize("fixed_val, grid_cell", FIXED_VAL_CASES)
+def test_init_fixed_val(fixed_val, grid_cell):
     """
-    Test output of movie_ratings_over_time function which returns a
-    dictionary containing all the movie ratings over an actor's career.
+    Test that board initializes with proper fixed values
     """
-    assert ratings_test == ratings_result
+    board = Board(PUZZLE)
+    assert board.fixedValues[grid_cell[0], grid_cell[1]] == fixed_val
+
+@pytest.mark.parametrize("board_val, grid_cell", GET_VAL_CASES)
+def test_getVal(board_val, grid_cell):
+    """
+    Test the getVal function which returns the value at a given row and column
+    """
+    assert board_val == TEST_BOARD.getVal(grid_cell[0], grid_cell[1])
+
+@pytest.mark.parametrize("exception, grid_cell", GET_VAL_EXCEPTION_CASES)
+def test_getVal_exception(exception, grid_cell):
+    """
+    Test that the getVal function raises an exception when an invalid row or column is passed
+    """
+    with pytest.raises(exception):
+        TEST_BOARD.getVal(grid_cell[0], grid_cell[1])
+
+@pytest.mark.parametrize("board_val, grid_cell", SET_VAL_CASES)
+def test_setVal(board_val, grid_cell):
+    """
+    Test the setVal function which sets the value at a given row and column
+    """
+    temp_board = Board(PUZZLE)
+    temp_board.setVal(grid_cell[0], grid_cell[1], board_val)
+    assert board_val == temp_board.getVal(grid_cell[0], grid_cell[1])
+
+@pytest.mark.parametrize("exception, grid_cell", SET_VAL_EXCEPTION_CASES)
+def test_setVal_exception(exception, grid_cell):
+    """
+    Test that the setVal function raises an exception when an invalid row or column is passed
+    """
+    with pytest.raises(exception):
+        TEST_BOARD.setVal(grid_cell[0], grid_cell[1], 1)
+
+@pytest.mark.parametrize("row_val, row_num", GET_ROW_CASES)
+def test_getRow(row_val, row_num):
+    """
+    Test the getRow function by checking that a row is returned with all the right values.
+    """
+    board = Board(PUZZLE)
+    this_row = list(board.getRow(row_num))
+    assert this_row == row_val
+
+@pytest.mark.parametrize("row_val, row_num", GET_ROW_CASES)
+def test_getRow_shape(row_val, row_num):
+    """
+    Test the getRow function by checking that a row is returned with the right shape.
+    """
+    board = Board(PUZZLE)
+    this_row = board.getRow(row_num)
+    assert this_row.shape == (9, )
+
+@pytest.mark.parametrize("col_val, col_num", GET_COL_CASES)
+def test_getCol(col_val, col_num):
+    """
+    Test the getCol function by checking that a column is returned with all the right values.
+    """
+    board = Board(PUZZLE)
+    this_col = list(board.getCol(col_num))
+    assert this_col == col_val
+
+@pytest.mark.parametrize("col_val, col_num", GET_COL_CASES)
+def test_getCol_shape(col_val, col_num):
+    """
+    Test the getCol function by checking that a column is returned with the right shape.
+    """
+    board = Board(PUZZLE)
+    this_col = board.getCol(col_num)
+    assert this_col.shape == (9, )
+
+@pytest.mark.parametrize("subgrid_val, position", GET_SUBGRID_CASES)
+def test_getSubgrid(subgrid_val, position):
+    """
+    """
+    board = Board(PUZZLE)
+    this_subgrid = list(board.getSubgrid(position[0], position[1]))
+    assert this_subgrid == subgrid_val
+
